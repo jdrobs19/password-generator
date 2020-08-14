6eec38c9d56dd72cec2f118447adcc7e38d1c668
Jordan Roberts <jdrobs19@gmail.com>
2020-08-14 13:51:48 -0600

    set up character arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+function generatePassword(){
+
+  var passLength = prompt("What would you like the length of your password to be between 8-128 characters?");
+
+  if(passLength < 8 || passLength > 128){
+    alert("Password must be between 8-128 characters.");
+  }
+
+  
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
